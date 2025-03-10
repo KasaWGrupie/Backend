14f6e67a60787c1f73005b51a7971a8d3b7f3bf2
Igor Januszkiewicz <116904328+IJanuszkiewicz@users.noreply.github.com>
2025-03-10 09:07:14 +0100

    Api testing templates
    * Generate controllers based on api
    
    * Generate api tests

---
+++ KasaWGrupieBackendTests/Controllers/ControllerTests.cs
@@ -0,0 +1,236 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KasaWGrupieBackend.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KasaWGrupieBackend.Controllers.Tests
+{
+    [TestClass()]
+    public class ControllerTests
+    {
+        [TestMethod()]
+        public void ControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateUserTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetUserByIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteUserTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ChangeUserNameTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ChangeProfilePictureTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AuthenticateUserTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetUsersFriendsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FriendRequestsAllTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FriendRequestsPUTTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FriendRequestsPOSTTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetUserByEmailTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ExpensesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GroupsAllTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MoneyRequestPOSTTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MoneyRequestPUTTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MoneyRequestPUT2Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MoneyRequestDELETETest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FindBySenderTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FindByRecipientTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ReceiptTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddExpenseTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditExpenseTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetGroupBalancesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetUserBalancesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MoneyTransferTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void StatusTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GroupsGETTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GroupsPOSTTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void Status2Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void JoinRequestsAllTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void JoinRequestsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GroupInvitesAllTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GroupInvitesPOSTTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GroupInvitesPUTTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void InviteCodeGETTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void InviteCodePUTTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
