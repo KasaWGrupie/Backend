dea7fb2c48d1140f0fb5a2aa072561439a735976
Antonina Frackowiak <160714681+tosiaf@users.noreply.github.com>
2025-03-15 19:43:51 +0100

    Dodanie docker-compose i obsługi secretów (#40)

---
+++ KasaWGrupieBackend/DependencyInjection.cs
@@ -0,0 +1,10 @@
+﻿namespace KasaWGrupie.API;
+
+public static class DependencyInjection
+{
+	public static IServiceCollection RegisterConfigurationOptions(this IServiceCollection services, IConfiguration configuration)
+	{
+		//services.Configure<SecretClass>(configuration.GetSection(SecretClass.SectionName));
+		return services;
+	}
+}
