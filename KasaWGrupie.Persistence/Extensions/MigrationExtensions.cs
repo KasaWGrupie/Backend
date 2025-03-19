6943ea57727f871cf755524e4e374f396179174e
Antonina Frackowiak <160714681+tosiaf@users.noreply.github.com>
2025-03-19 20:55:12 +0100

    Feature/16-integracja-z-bazą-danych (#41)
    * Added migration and dbcontext
    
    * Jakies bledy mam :((
    
    * Działa baza jeej
    
    * Readme update
    
    * Cleanup

---
+++ KasaWGrupie.Persistence/Extensions/MigrationExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using KasaWGrupie.Persistence.Data;
+
+namespace KasaWGrupie.Persistence.Extensions;
+
+public static class MigrationExtensions
+{
+	public static void ApplyMigrations(this IServiceProvider serviceProvider)
+	{
+		using IServiceScope scope = serviceProvider.CreateScope();
+		var dbContext = scope.ServiceProvider.GetRequiredService<KasaWGrupieDbContext>();
+
+		dbContext.Database.Migrate();
+	}
+}
