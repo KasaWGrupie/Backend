7969a343681abc34ff311519c70b17f71928ff90
xblmati <mraczynski01@gmail.com>
2025-03-23 12:23:22 +0100

    Package fixes

--- KasaWGrupie.Infrastructure/AuthService/AuthService.cs
+++ KasaWGrupie.Infrastructure/AuthService/AuthService.cs
@@ -1,3 +1,4 @@
+using FirebaseAdmin;
 using FirebaseAdmin.Auth;
 
 namespace KasaWGrupie.Infrastructure.AuthService;
@@ -13,7 +14,7 @@ public class AuthService : IAuthService
                 .VerifyIdTokenAsync(idToken, cancellationToken);
             return AuthenticationResult.Success(decodedToken);
         }
-        catch (FirebaseAuthException e)
+        catch (FirebaseException e)
         {
             return AuthenticationResult.Failure(e);
         }
