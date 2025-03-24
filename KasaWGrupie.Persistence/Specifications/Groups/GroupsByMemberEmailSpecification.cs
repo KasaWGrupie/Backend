bd17c362e83795374aef206808f15e2c17bed2d7
2ffifee <133985403+2ffifee@users.noreply.github.com>
2025-03-24 09:09:52 +0100

    Endpoint do pobierania grup uzytkownika (#47)

---
+++ KasaWGrupie.Persistence/Specifications/Groups/GroupsByMemberEmailSpecification.cs
@@ -0,0 +1,24 @@
+﻿using Ardalis.Specification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using KasaWGrupie.Core.Entities;
+
+namespace KasaWGrupie.Persistence.Specifications.Groups
+{
+    public class GroupsByMemberEmailSpecification : Specification<Group>
+    {
+        public GroupsByMemberEmailSpecification(string userEmail)
+        {
+            Query
+                .Include(g => g.Members)
+                .Include(g => g.Currency)
+                .Include(g => g.Admin)
+                .Where(g => g.Members.Any(m => m.Email == userEmail));
+        }
+    }
+
+}
---
+++ KasaWGrupieBackend/DTOs/Users/GetUserGroupsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace KasaWGrupie.API.DTOs.Users;
+
+public sealed record GetUserGroupsDto(
+    int Id,
+    string Name,
+    string Currency,
+    string AdminEmail
+);
+
