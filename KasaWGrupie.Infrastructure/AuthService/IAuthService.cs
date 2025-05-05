d460927ad21530f240be37a2a2513aa68e73bc1a
xblmati <mraczynski01@gmail.com>
2025-05-05 08:26:52 +0200

    Naprawa błędu przy tworzeniu UsersController

--- KasaWGrupie.Infrastructure/AuthService/IAuthService.cs
+++ KasaWGrupie.Infrastructure/AuthService/IAuthService.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore.Http;
+
 namespace KasaWGrupie.Infrastructure.AuthService;
 
 public interface IAuthService
 {
     Task<AuthenticationResult> AuthenticateAsync(string idToken, CancellationToken cancellationToken);
+    Task<string> GetEmailFromAuthTokenAsync(HttpContext context, CancellationToken cancellationToken);
 }
