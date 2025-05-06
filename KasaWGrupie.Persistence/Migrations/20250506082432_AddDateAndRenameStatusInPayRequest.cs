f082670cbf7e10b67bee5c603f2aded5ccce10e8
xblmati <mraczynski01@gmail.com>
2025-05-06 10:25:51 +0200

    Migration

---
+++ KasaWGrupie.Persistence/Migrations/20250506082432_AddDateAndRenameStatusInPayRequest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KasaWGrupie.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDateAndRenameStatusInPayRequest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "payRequstStatus",
+                table: "PayRequests",
+                newName: "PayRequestStatus");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                table: "PayRequests",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "PayRequests");
+
+            migrationBuilder.RenameColumn(
+                name: "PayRequestStatus",
+                table: "PayRequests",
+                newName: "payRequstStatus");
+        }
+    }
+}
