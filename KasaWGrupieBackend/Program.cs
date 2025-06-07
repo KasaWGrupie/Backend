bab852a51e1e321e386c6259ba3692469370661d
Filip <filip.filipkowski6@gmail.com>
2025-06-07 18:09:39 +0200

    usuniecie niepotrzebnych linii

--- KasaWGrupieBackend/Program.cs
+++ KasaWGrupieBackend/Program.cs
@@ -27,7 +27,6 @@ public class Program
 		// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 		builder.Services.AddEndpointsApiExplorer();
 		builder.Services.AddSwaggerGen();
-        builder.Configuration.AddEnvironmentVariables();
 
 
         var app = builder.Build();
