3e762b09134e85390fc39edff4e812f94430aa60
Antonina Frackowiak <160714681+tosiaf@users.noreply.github.com>
2025-06-09 00:45:08 +0200

    Zmiana create expense

--- KasaWGrupieBackend/DTOs/Expense/CreateExpenseDto.cs
+++ KasaWGrupieBackend/DTOs/Expense/CreateExpenseDto.cs
@@ -1,13 +1,12 @@
 ﻿namespace KasaWGrupie.API.DTOs.Expense;
 
 public sealed record CreateExpenseDto(
-    int GroupId,
-    int PaidBy,
-    string ExpenseName,
-    string ExpensePictureUri,
-    string Description,
-    decimal Amount,
-    DateTime Date,
-    List<ExpenseParticipantDto> Participants,
-    string DivisionMethod
+	int GroupId,
+	int PaidBy,
+	string ExpenseName,
+	string Description,
+	decimal Amount,
+	DateTime Date,
+	List<ExpenseParticipantDto> Participants,
+	string DivisionMethod
 );
