0aa248d7a9485673e18c606753f41cc15cbedfc0
Antonina Frackowiak <160714681+tosiaf@users.noreply.github.com>
2025-06-08 21:27:52 +0200

    Endpointy do invite code

--- KasaWGrupieBackend/Controllers/GroupsController.cs
+++ KasaWGrupieBackend/Controllers/GroupsController.cs
@@ -21,7 +21,7 @@ public sealed class GroupsController : ControllerBase
 		_mediator = mediator;
 		_authService = authService;
 	}
-	
+
 	private static readonly JsonSerializerOptions JsonOptions = new() { PropertyNameCaseInsensitive = true };
 
 	/// <summary>
@@ -43,7 +43,7 @@ public sealed class GroupsController : ControllerBase
 		{
 			return Result.Invalid(new ValidationError("dto", "Invalid JSON data."));
 		}
-		
+
 		var userEmail = await _authService.GetEmailFromAuthTokenAsync(HttpContext, HttpContext.RequestAborted);
 		var command = new CreateGroupCommand(userEmail, createGroupDto, image);
 		var result = await _mediator.Send(command);
@@ -58,83 +58,97 @@ public sealed class GroupsController : ControllerBase
 		var userId = await _authService.GetUserIdFromAuthTokenAsync(HttpContext);
 		var command = new GetExpensesCommand(userId, groupId);
 		var result = await _mediator.Send(command);
-		
+
 		return result;
 	}
-	
-    [TranslateResultToActionResult]
-    [HttpPut("{groupId}")]
-    [Consumes("multipart/form-data")]
-    public async Task<Result> UpdateGroup([FromRoute] int groupId, IFormCollection formCollection, IFormFile? image)
-    {
-	    var json = formCollection["dto"][0];
-	    if (json is null)
-	    {
-		    return Result.Invalid(new ValidationError("dto", "JSON data required in 'dto' field."));
-	    }
-	    
-	    var updateGroupDto = JsonSerializer.Deserialize<UpdateGroupDto>(json, JsonOptions);
-	    if (updateGroupDto is null)
-	    {
-		    return Result.Invalid(new ValidationError("dto", "Invalid JSON data."));
-	    }
-	    
-	    var userId = await _authService.GetUserIdFromAuthTokenAsync(HttpContext);
-        var command = new UpdateGroupCommand(userId, groupId, updateGroupDto, image);
-        var result = await _mediator.Send(command);
-        return result;
-    }
-    
-    [TranslateResultToActionResult]
-    [HttpPut("{groupId:int}/status")]
-    public async Task<Result> ChangeStatus(
+
+	[TranslateResultToActionResult]
+	[HttpPut("{groupId}")]
+	[Consumes("multipart/form-data")]
+	public async Task<Result> UpdateGroup([FromRoute] int groupId, IFormCollection formCollection, IFormFile? image)
+	{
+		var json = formCollection["dto"][0];
+		if (json is null)
+		{
+			return Result.Invalid(new ValidationError("dto", "JSON data required in 'dto' field."));
+		}
+
+		var updateGroupDto = JsonSerializer.Deserialize<UpdateGroupDto>(json, JsonOptions);
+		if (updateGroupDto is null)
+		{
+			return Result.Invalid(new ValidationError("dto", "Invalid JSON data."));
+		}
+
+		var userId = await _authService.GetUserIdFromAuthTokenAsync(HttpContext);
+		var command = new UpdateGroupCommand(userId, groupId, updateGroupDto, image);
+		var result = await _mediator.Send(command);
+		return result;
+	}
+
+	[TranslateResultToActionResult]
+	[HttpPut("{groupId:int}/status")]
+	public async Task<Result> ChangeStatus(
   [FromRoute] int groupId,
   [FromBody] ChangeGroupStatusDto dto)
-    {
-      // merge route + body into one DTO:
-        var dtoWithId = dto with { GroupId = groupId };
-        var cmd = new ChangeGroupStatusCommand(dtoWithId);
-        return await _mediator.Send(cmd);
-    }
-  
-  
-    [TranslateResultToActionResult]
-    [HttpGet("{groupId:int}")]
-    public async Task<Result<GroupDto>> GetGroupById([FromRoute] int groupId)
-    {
-        var cmd = new GetGroupByIdCommand(groupId);
-        return await _mediator.Send(cmd);
-    }
-    
-    [TranslateResultToActionResult]
-    [HttpGet("{groupId:int}/balances")]
-    public async Task<Result<GetGroupBalancesDto>> GetBalances(
+	{
+		// merge route + body into one DTO:
+		var dtoWithId = dto with { GroupId = groupId };
+		var cmd = new ChangeGroupStatusCommand(dtoWithId);
+		return await _mediator.Send(cmd);
+	}
+
+
+	[TranslateResultToActionResult]
+	[HttpGet("{groupId:int}")]
+	public async Task<Result<GroupDto>> GetGroupById([FromRoute] int groupId)
+	{
+		var cmd = new GetGroupByIdCommand(groupId);
+		return await _mediator.Send(cmd);
+	}
+
+	[TranslateResultToActionResult]
+	[HttpGet("{groupId:int}/balances")]
+	public async Task<Result<GetGroupBalancesDto>> GetBalances(
 [FromRoute] int groupId)
-    {
-        var cmd = new GetGroupBalancesCommand(groupId);
-        return await _mediator.Send(cmd);
-    }
+	{
+		var cmd = new GetGroupBalancesCommand(groupId);
+		return await _mediator.Send(cmd);
+	}
+
+	[TranslateResultToActionResult]
+	[HttpPut("inviteCode")]
+	public async Task<Result> RequestToJoinGroupWithInviteCode([FromBody] InviteCodeDto updateInviteCodeDto)
+	{
+		var userEmail = await _authService.GetEmailFromAuthTokenAsync(HttpContext, HttpContext.RequestAborted);
+		var command = new RequestToJoinGroupWithInviteCodeCommand(updateInviteCodeDto, userEmail);
+		var result = await _mediator.Send(command);
+		return result;
+	}
+
+	[TranslateResultToActionResult]
+	[HttpGet("{groupId:int}/joinRequests")]
+	public async Task<Result<GetGroupJoinRequestsDto>> GetJoinRequests(
+	 [FromRoute] int groupId)
+	 => await _mediator.Send(new GetGroupJoinRequestsCommand(groupId));
 
 
+	[TranslateResultToActionResult]
+	[HttpPut("{groupId:int}/joinRequests/{requestId:int}")]
+	public async Task<Result> ChangeJoinRequestStatus(
+	[FromRoute] int groupId,
+	[FromRoute] int requestId,
+	[FromBody] ChangeJoinRequestStatusDto dto)
+	{
+		var cmd = new ChangeJoinRequestStatusCommand(groupId, requestId, dto.Status);
+		return await _mediator.Send(cmd);
+	}
 
-
-
-    [TranslateResultToActionResult]
-    [HttpGet("{groupId:int}/joinRequests")]
-    public async Task<Result<GetGroupJoinRequestsDto>> GetJoinRequests(
-  [FromRoute] int groupId)
-  => await _mediator.Send(new GetGroupJoinRequestsCommand(groupId));
-
-
-    [TranslateResultToActionResult]
-    [HttpPut("{groupId:int}/joinRequests/{requestId:int}")]
-    public async Task<Result> ChangeJoinRequestStatus(
-  [FromRoute] int groupId,
-  [FromRoute] int requestId,
-  [FromBody] ChangeJoinRequestStatusDto dto)
-    {
-        var cmd = new ChangeJoinRequestStatusCommand(groupId, requestId, dto.Status);
-        return await _mediator.Send(cmd);
-    }
-
+	[TranslateResultToActionResult]
+	[HttpGet("{groupId}/inviteCode")]
+	public async Task<Result<InviteCodeDto>> GetGroupInviteCode(int groupId)
+	{
+		var command = new GetGroupInviteCodeCommand(groupId);
+		var result = await _mediator.Send(command);
+		return result;
+	}
 }
