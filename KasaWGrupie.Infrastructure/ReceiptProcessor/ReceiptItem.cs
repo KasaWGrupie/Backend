2de97325600147932859693b26e33d5f591ed96e
2ffifee <133985403+2ffifee@users.noreply.github.com>
2025-06-09 02:14:51 +0200

    Czytanie z paragonów

---
+++ KasaWGrupie.Infrastructure/ReceiptProcessor/ReceiptItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KasaWGrupie.Infrastructure.ReceiptProcessor
+{
+    public class ReceiptItem
+    {
+        public string Description { get; set; } = "";
+        public double Quantity { get; set; }
+        public double Price { get; set; }
+    }
+}
